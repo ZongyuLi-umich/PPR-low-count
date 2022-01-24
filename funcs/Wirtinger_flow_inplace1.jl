e04e9d68388b084d28de15da0ccc4a9ec348f608
Zongyu Li <54018575+ZongyuLi-umich@users.noreply.github.com>
2022-01-24 15:08:22 -0500

    wf inplace 1

--- funcs/Wirtinger_flow_inplace1.jl
+++ funcs/Wirtinger_flow_inplace1.jl
@@ -157,54 +157,6 @@ function update_state!(A, y, b,
 end
 
 
-function select_root(roots, state)
-    if any(isnan.(roots))
-        return roots[1]
-    else
-        broadcast!(+, state.Ax_old, state.Ax, roots[1] * state.Adk)
-        broadcast!(+, state.Ax_new, state.Ax, roots[2] * state.Adk)
-        broadcast!(+, state.work_Ax1, state.Ax, roots[3] * state.Adk)
-
-        cf1 = state.cost_fun(state.cost_fun_vec, state.work_Ax, state.Ax_old, y, b)
-        cf2 = state.cost_fun(state.cost_fun_vec, state.work_Ax, state.Ax_new, y, b)
-        cf3 = state.cost_fun(state.cost_fun_vec, state.work_Ax, state.work_Ax1, y, b)
-        return roots[argmin([cf1, cf2, cf3])]
-    end
-
-end
-
-
-function cubic(a, b, c)
-#
-# This function extracts the real roots of the cubic equation
-# x^3+a*x^2+b*x+c = 0.
-#
-# Check the discriminant condition.
-#
-   q = (a*a - 3.0 * b)/ 9.0
-   r = (2.0 * a^3 - 9.0 * a * b + (3.0 * 9.0) * c)/ (9.0 * 6.0)
-   if r^2 < q^3
-#
-# Three real roots.
-#
-      theta = acos(r / sqrt(q^3))
-      return [-2.0 * sqrt(q) * cos(theta / 3.0) - a / 3.0
-      -2.0 * sqrt(q) * cos((theta + 2.0 * pi) / 3.0) - a / 3.0
-      -2.0 * sqrt(q) * cos((theta - 2.0 * pi) / 3.0) - a / 3.0]
-   else
-#
-# A single real root.
-#
-      d = -sign(r) * cbrt(abs(r) + sqrt(r * r - q^3))
-      if abs(d) <= 0.0
-         e = 0.0
-      else
-         e = q/d
-      end
-      return [d + e - a / 3.0, NaN, NaN]
-   end
-end # function cubic
-
 function Wirtinger_flow_inplace1!(A, y, b,
                                   niter::Int,
                                   state::WFState;
